86e356a6f50a512c4dfcda7be9655f283c2e3a42
FreddyMutant <fedorasan@mail.ru>
2023-08-31 18:46:04 +0300

    13pr last

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,16 +1,19 @@
-const mongoose = require('mongoose');
-const Card = require('../models/card');
+const mongoose = require("mongoose");
+const Card = require("../models/card");
 const {
   VALIDATION_ERROR,
   NOT_FOUND_ERROR,
   REFERENCE_ERROR,
-} = require('../errors/errorsCodes');
+} = require("../errors/errorsCodes");
 
 module.exports.getCards = (req, res) => {
   Card.find({})
-    .populate('owner')
     .then((cards) => res.send({ data: cards }))
-    .catch(() => res.status(REFERENCE_ERROR).send({ message: 'Произошла ошибка по умолчанию' }));
+    .catch(() =>
+      res
+        .status(REFERENCE_ERROR)
+        .send({ message: "Произошла ошибка по умолчанию" })
+    );
 };
 
 module.exports.createCard = (req, res) => {
@@ -20,9 +23,13 @@ module.exports.createCard = (req, res) => {
     .then((card) => res.send({ data: card }))
     .catch((err) => {
       if (err instanceof mongoose.Error.ValidationError) {
-        res.status(VALIDATION_ERROR).send({ message: 'Переданы некорректные данные при создании карточки' });
+        res.status(VALIDATION_ERROR).send({
+          message: "Переданы некорректные данные при создании карточки",
+        });
       } else {
-        res.status(REFERENCE_ERROR).send({ message: 'Произошла ошибка по умолчанию' });
+        res
+          .status(REFERENCE_ERROR)
+          .send({ message: "Произошла ошибка по умолчанию" });
       }
     });
 };
@@ -31,57 +38,77 @@ module.exports.deleteCardById = (req, res, next) => {
   Card.findByIdAndRemove(req.params.cardId)
     .then((card) => {
       if (!card) {
-        res.status(NOT_FOUND_ERROR).send({ message: 'Карточка с указанным _id не найдена.' });
+        res
+          .status(NOT_FOUND_ERROR)
+          .send({ message: "Карточка с указанным _id не найдена." });
       } else {
-        next(res.send({ data: card }));
+        res.send({ data: card });
       }
     })
     .catch((err) => {
       if (err instanceof mongoose.Error.CastError) {
-        res.status(VALIDATION_ERROR).send({ message: 'Переданы некорректные данные для удаления карточки' });
+        res.status(VALIDATION_ERROR).send({
+          message: "Переданы некорректные данные для удаления карточки",
+        });
       } else {
-        res.status(REFERENCE_ERROR).send({ message: 'Произошла ошибка по умолчанию' });
+        res
+          .status(REFERENCE_ERROR)
+          .send({ message: "Произошла ошибка по умолчанию" });
       }
     });
 };
 
-module.exports.likeCard = (req, res, next) => Card.findByIdAndUpdate(
-  req.params.cardId,
-  { $addToSet: { likes: req.user._id } },
-  { new: true },
-  { runValidators: true },
-)
-  .then((card) => {
-    if (!card) {
-      res.status(NOT_FOUND_ERROR).send({ message: 'Переданы некорректные данные для постановки лайка.' });
-    } else {
-      next(res.send({ data: card }));
-    }
-  })
-  .catch((err) => {
-    if (err instanceof mongoose.Error.CastError) {
-      res.status(VALIDATION_ERROR).send({ message: 'Передан несуществующий _id карточки.' });
-    } else {
-      res.status(REFERENCE_ERROR).send({ message: 'Произошла ошибка по умолчанию' });
-    }
-  });
+module.exports.likeCard = (req, res, next) =>
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } },
+    { new: true },
+    { runValidators: true }
+  )
+    .then((card) => {
+      if (!card) {
+        res.status(NOT_FOUND_ERROR).send({
+          message: "Переданы некорректные данные для постановки лайка.",
+        });
+      } else {
+        res.send({ data: card });
+      }
+    })
+    .catch((err) => {
+      if (err instanceof mongoose.Error.CastError) {
+        res
+          .status(VALIDATION_ERROR)
+          .send({ message: "Передан несуществующий _id карточки." });
+      } else {
+        res
+          .status(REFERENCE_ERROR)
+          .send({ message: "Произошла ошибка по умолчанию" });
+      }
+    });
 
-module.exports.dislikeCard = (req, res, next) => Card.findByIdAndUpdate(
-  req.params.cardId,
-  { $pull: { likes: req.user._id } },
-  { new: true },
-)
-  .then((card) => {
-    if (!card) {
-      res.status(NOT_FOUND_ERROR).send({ message: 'Передан несуществующий _id карточки.' });
-    } else {
-      next(res.send({ data: card }));
-    }
-  })
-  .catch((err) => {
-    if (err instanceof mongoose.Error.CastError) {
-      res.status(VALIDATION_ERROR).send({ message: 'Переданы некорректные данные для снятия лайка.' });
-    } else {
-      res.status(REFERENCE_ERROR).send({ message: 'Произошла ошибка по умолчанию' });
-    }
-  });
+module.exports.dislikeCard = (req, res, next) =>
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } },
+    { new: true }
+  )
+    .then((card) => {
+      if (!card) {
+        res
+          .status(NOT_FOUND_ERROR)
+          .send({ message: "Передан несуществующий _id карточки." });
+      } else {
+        res.send({ data: card });
+      }
+    })
+    .catch((err) => {
+      if (err instanceof mongoose.Error.CastError) {
+        res
+          .status(VALIDATION_ERROR)
+          .send({ message: "Переданы некорректные данные для снятия лайка." });
+      } else {
+        res
+          .status(REFERENCE_ERROR)
+          .send({ message: "Произошла ошибка по умолчанию" });
+      }
+    });
--- controllers/users.js
+++ controllers/users.js
@@ -1,15 +1,19 @@
-const mongoose = require('mongoose');
-const User = require('../models/user');
+const mongoose = require("mongoose");
+const User = require("../models/user");
 const {
   VALIDATION_ERROR,
   NOT_FOUND_ERROR,
   REFERENCE_ERROR,
-} = require('../errors/errorsCodes');
+} = require("../errors/errorsCodes");
 
 module.exports.getUsers = (req, res) => {
   User.find({})
     .then((users) => res.send({ data: users }))
-    .catch(() => res.status(REFERENCE_ERROR).send({ message: 'Произошла ошибка по умолчанию' }));
+    .catch(() =>
+      res
+        .status(REFERENCE_ERROR)
+        .send({ message: "Произошла ошибка по умолчанию" })
+    );
 };
 
 module.exports.createUser = (req, res) => {
@@ -19,9 +23,13 @@ module.exports.createUser = (req, res) => {
     .then((user) => res.send({ data: user }))
     .catch((err) => {
       if (err instanceof mongoose.Error.ValidationError) {
-        res.status(VALIDATION_ERROR).send({ message: 'Переданы некорректные данные при создании пользователя' });
+        res.status(VALIDATION_ERROR).send({
+          message: "Переданы некорректные данные при создании пользователя",
+        });
       } else {
-        res.status(REFERENCE_ERROR).send({ message: 'Произошла ошибка по умолчанию' });
+        res
+          .status(REFERENCE_ERROR)
+          .send({ message: "Произошла ошибка по умолчанию" });
       }
     });
 };
@@ -30,34 +38,25 @@ module.exports.getUserById = (req, res, next) => {
   User.findById(req.params.userId)
     .then((user) => {
       if (!user) {
-        res.status(NOT_FOUND_ERROR).send({ message: 'Пользователь по указанному _id не найден.' });
+        res
+          .status(NOT_FOUND_ERROR)
+          .send({ message: "Пользователь по указанному _id не найден." });
       } else {
-        next(res.send({ data: user }));
+        res.send({ data: user });
       }
     })
     .catch((err) => {
       if (err instanceof mongoose.Error.CastError) {
-        res.status(VALIDATION_ERROR).send({ message: 'Переданы некорректные данные _id' });
+        res
+          .status(VALIDATION_ERROR)
+          .send({ message: "Переданы некорректные данные _id" });
       } else {
-        res.status(REFERENCE_ERROR).send({ message: 'Произошла ошибка по умолчанию' });
+        res
+          .status(REFERENCE_ERROR)
+          .send({ message: "Произошла ошибка по умолчанию" });
       }
     });
 };
-
-function cachingDecorator(func) {
-  const cache = new Map();
-
-  return function (x) {
-    if (cache.has(x)) {
-      return cache.get(x);
-    }
-
-    const result = func(x);
-
-    cache.set(x, result);
-    return result;
-  };
-}
 
 function updateUserData(req, res, next, args) {
   User.findByIdAndUpdate(req.user._id, args, { new: true, runValidators: true })
@@ -65,21 +64,27 @@ function updateUserData(req, res, next, args) {
     .then((user) => next(res.send({ data: user })))
     .catch((err) => {
       if (err instanceof mongoose.Error.DocumentNotFoundError) {
-        res.status(NOT_FOUND_ERROR).send({ message: 'Пользователь с указанным _id не найден.' });
+        res
+          .status(NOT_FOUND_ERROR)
+          .send({ message: "Пользователь с указанным _id не найден." });
       } else if (err instanceof mongoose.Error.ValidationError) {
-        res.status(VALIDATION_ERROR).send({ message: 'Переданы некорректные данные при обновлении профиля.' });
+        res.status(VALIDATION_ERROR).send({
+          message: "Переданы некорректные данные при обновлении профиля.",
+        });
       } else {
-        res.status(REFERENCE_ERROR).send({ message: 'Произошла ошибка по умолчанию' });
+        res
+          .status(REFERENCE_ERROR)
+          .send({ message: "Произошла ошибка по умолчанию" });
       }
     });
 }
 
 module.exports.updateProfile = (req, res, next) => {
   const { name, about } = req.body;
-  cachingDecorator(updateUserData(req, res, next, { name, about }));
+  updateUserData(req, res, next, { name, about });
 };
 
 module.exports.updateAvatar = (req, res, next) => {
   const { avatar } = req.body;
-  cachingDecorator(updateUserData(req, res, next, { avatar }));
+  updateUserData(req, res, next, { avatar });
 };
